3553800cbf752534ad098b1bd186dccc931cef4a
hiuzz24 <hieupthe181180@fpt.edu.vn>
2025-11-25 15:11:41 +0700

    check

---
+++ src/main/java/pacman/plantmarket/entity/Role.java
@@ -0,0 +1,21 @@
+package pacman.plantmarket.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "role_name",unique = true,nullable = false)
+    private String roleName;
+}
