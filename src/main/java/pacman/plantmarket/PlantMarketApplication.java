fb236d61078145b264e0b330d098a07c8b7cd299
hiuzz24 <hieupthe181180@fpt.edu.vn>
2025-11-25 14:51:39 +0700

    check

---
+++ src/main/java/pacman/plantmarket/PlantMarketApplication.java
@@ -0,0 +1,13 @@
+package pacman.plantmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pacman/plantmarket/PlantMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pacman.plantmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
